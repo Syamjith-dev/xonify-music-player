a15e01b04107555470760ce276165e1d379bd2b9
syamjith <syamjithloq@gmail.com>
2026-03-20 14:01:55 +0530

    update fileupload

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'YOUR_CLOUD_NAME',
-  api_key: 'YOUR_API_KEY',
-  api_secret: 'YOUR_API_SECRET'
+  cloud_name: 'dmwdgiyey',
+  api_key: '384252811251129',
+  api_secret: 'pHLlyobbSog6IxmGMH5xp44Tj0M'
 });
 
 module.exports = cloudinary;
